23bc759fcebdf8b82e887d4e3e914d6eb63b00c5
Joseph Ralph <josephralph4@gmail.com>
2016-12-16 10:23:07 +0000

    Updating ui.

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,9 @@
 <div class="row">
     <div class="col-lg-8 col-lg-offset-2 col-md-6 col-md-offset-3">
         <div class="header">
-            <h1 class="header__title">
+            <a class="header__title" href="{{ url('/') }}">
                 DB Diff
-            </h1>
+            </a>
         </div>
     </div>
 </div>
