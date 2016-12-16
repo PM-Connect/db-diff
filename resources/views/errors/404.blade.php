8f2b7ab3bf0c53d34284f27fbcf644c10def6325
Joseph Ralph <josephralph4@gmail.com>
2016-12-16 10:30:54 +0000

    Adding styled 404 page.

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="row">
+        <h1 class="error-404">
+            404
+            <small>Unable to find the page you are looking for!</small>
+        </h1>
+    </div>
+@endsection
